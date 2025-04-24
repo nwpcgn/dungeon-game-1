e26d33e5ef442a9cad9cbc7027e44c581773f4a3
nwpcgn <119536313+nwpcgn@users.noreply.github.com>
2025-04-24 17:53:52 +0200

    initial

---
+++ src/routes/tools/rot1/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Grid from '$lib/rogue/Rogue.svelte'
+	import Notifications from '$lib/toast/Notifications.svelte';
+</script>
+
+<section class="page nwp">
+	<Grid></Grid>
+</section>
+
+
+<Notifications></Notifications>
